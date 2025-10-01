745796e4f14ead0434bb77dab8dfc0c1911020f2
Paul <paulabes@users.noreply.github.com>
2025-10-01 19:52:27 +0100

    email fixes

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo json_encode([
-    'status' => 'success',
-    'message' => 'PHP is working correctly',
-    'timestamp' => date('Y-m-d H:i:s'),
-    'php_version' => PHP_VERSION
-]);
-?>
